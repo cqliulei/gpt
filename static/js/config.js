15e4fff356dde3f08d4522ba9075c8c70c85edbf
liulei <real_life@qq.com>
2023-05-16 15:41:03 +0800

    gpt web submit

---
+++ static/js/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: liulei real_life@qq.com
+ * @Date: 2023-05-16 15:24:00
+ * @LastEditors: liulei real_life@qq.com
+ * @LastEditTime: 2023-05-16 15:39:13
+ * @FilePath: \nxcgpt\static\js\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const config = {
+    // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
+    url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
+    apiKey: "c2stUWhIbVlzc3V3a0F1b0F2MEFIa01UM0JsYmtGSkt6S1o1Sng1cE5UV3l6dlRFb1V1"  // openAi Base64 编码 apiKey
+};  
